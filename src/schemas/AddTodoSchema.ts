e602a502cb0e0db03ed5e2a6ec13086641523e89
aviv <aviva@deepsea.group>
2025-10-13 11:43:17 +0300

    added customized error messages - zod schema

--- src/schemas/AddTodoSchema.ts
+++ src/schemas/AddTodoSchema.ts
@@ -2,10 +2,10 @@ import z from "zod";
 
 export const TodoItemSchema = z.object({
   id: z.string(),
-  text: z.string().min(2),
+  text: z.string().min(2, { error: "Too short!" }),
   completed: z.boolean(),
   createdAt: z.date(),
-  priority: z.string().min(2),
+  priority: z.string().min(2, { error: "Too short!" }),
 });
 
 export type TodoItemType = z.infer<typeof TodoItemSchema>;
