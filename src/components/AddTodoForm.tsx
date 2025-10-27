362269d5e756d28727df50f106e02dc8578f3b51
Aviv <aviv@Avivs-MacBook-Pro.local>
2025-10-27 12:37:04 +0200

    added react-hook-form's logic to addTodoForm

--- src/components/AddTodoForm.tsx
+++ src/components/AddTodoForm.tsx
@@ -1,4 +1,5 @@
-import { useRef, useState } from "react";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
 import { TodoItemSchema, TodoItemType } from "../schemas/AddTodoSchema";
 import classes from "./AddTodoForm.module.css";
 
@@ -8,54 +9,52 @@ type AddTodoFormProps = {
 };
 
 export const AddTodoForm = (props: AddTodoFormProps) => {
-  const textRef = useRef<HTMLInputElement>(null);
-  const priorityRef = useRef<HTMLInputElement>(null);
-  const [errors, setErrors] = useState<
-    Partial<Record<keyof TodoItemType, string>>
-  >({});
-  const handleSubmit = (e: React.FormEvent) => {
-    e.preventDefault();
-
-    const formData = {
-      text: textRef.current?.value || "",
-      priority: priorityRef.current?.value || "",
-      id: Math.random().toString(),
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+    reset,
+  } = useForm<TodoItemType>({
+    resolver: zodResolver(TodoItemSchema),
+    defaultValues: {
+      text: "",
       completed: false,
+      id: Math.random().toString(),
       createdAt: new Date(),
-    };
+      priority: "",
+    },
+  });
 
-    const result = TodoItemSchema.safeParse(formData);
-
-    if (!result.success) {
-      const fieldErrors: Partial<Record<keyof TodoItemType, string>> = {};
-      result.error.issues.forEach((error) => {
-        const field = error.path[0] as keyof TodoItemType;
-        if (field && !fieldErrors[field]) {
-          fieldErrors[field] = error.message;
-        }
-      });
-      setErrors(fieldErrors);
-      return;
-    }
-
-    setErrors({});
-    props.setTodoList([...props.todoList, formData]);
-
-    if (textRef.current) textRef.current.value = "";
-    if (priorityRef.current) priorityRef.current.value = "";
+  const onSubmit = (data: TodoItemType) => {
+    console.log("Todo data", data);
+    props.setTodoList([...props.todoList, data]);
+    reset();
   };
+
   return (
-    <form onSubmit={handleSubmit} className={classes.form}>
+    <form onSubmit={handleSubmit(onSubmit)} className={classes.form}>
       <h3>Add Todo</h3>
-      <div>
-        <label htmlFor="text">Add Text</label>
-        <input type="text" id="text" ref={textRef}></input>
-        {errors.text && <span className="error">{errors.text}</span>}
+      <div className={classes.formField}>
+        <div>
+          <label htmlFor="text">Add Text: </label>
+          <input type="text" id="text" {...register("text")}></input>
+        </div>
+        {errors.text && (
+          <span
+            className={classes.error}
+          >{`Text is ${errors.text.message}`}</span>
+        )}
       </div>
-      <div>
-        <label htmlFor="text">Add Priority</label>
-        <input type="text" id="priority" ref={priorityRef}></input>
-        {errors.priority && <span className="error">{errors.priority}</span>}
+      <div className={classes.formField}>
+        <div>
+          <label htmlFor="text">Add Priority: </label>
+          <input type="text" id="priority" {...register("priority")}></input>
+        </div>
+        {errors.priority && (
+          <span
+            className={classes.error}
+          >{`Priority is ${errors.priority.message}`}</span>
+        )}
       </div>
       <button type="submit">Submit</button>
     </form>
